9d631e44bce3686e542590fa0448d0ec2a5d54c0
Vince LaGrassa <vincent.lagrassa@gmail.com>
2017-12-11 15:55:45 -0500

    Add window loop to main

--- src/Main.cpp
+++ src/Main.cpp
@@ -18,4 +18,24 @@ int main() {
     
     std::cout << "Test 1: (" << test1.getPosition().x << ", " << test1.getPosition().y << ")\n";
     std::cout << "Test 2: (" << test2.getPosition().x << ", " << test2.getPosition().y << ")\n";
+    
+    sf::RenderWindow window(sf::VideoMode(1366, 768), "Latin Map Project");
+    
+    while (window.isOpen()) {
+
+        sf::Event event;
+        
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        
+        window.clear(sf::Color::White);
+        
+        window.draw(test1);
+        window.draw(test2);
+        
+        window.display();
+    }
 }
