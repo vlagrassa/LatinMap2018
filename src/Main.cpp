1fae7d7f69c9e99928fd7274fdf5636045475267
Vince LaGrassa <vincent.lagrassa@gmail.com>
2017-12-31 14:21:25 -0500

    Add testing for printing

--- src/Main.cpp
+++ src/Main.cpp
@@ -20,7 +20,13 @@ int main() {
     std::cout << "Test 1: (" << test1.getPosition().x << ", " << test1.getPosition().y << ")\n";
     std::cout << "Test 2: (" << test2.getPosition().x << ", " << test2.getPosition().y << ")\n";
     
-    sf::RenderWindow window(sf::VideoMode(1366, 768), "Latin Map Project");
+    Stack<MapPoint> testStack;
+    testStack.push(test1);
+    testStack.push(test2);
+    
+    std::cout << *testStack.top;
+    
+    //sf::RenderWindow window(sf::VideoMode(1366, 768), "Latin Map Project");
     
     //sf::Texture defaultPoint;
     //defaultPoint.loadFromFile("res/x.jpg"); //Image from https://www.freepik.com/free-icon/x-circle_692346.htm
@@ -28,15 +34,8 @@ int main() {
     //test1.setTexture(defaultPoint);
     //test2.setTexture(defaultPoint);
     
-    test1.setScale(0.05, 0.05);
-    test2.setScale(0.05, 0.05);
-    
-    Node<MapPoint> testNode1(test1);
-    Node<MapPoint> testNode2(test2);
-    testNode1.next = &testNode2;
-    Stack<MapPoint> testStack(testNode1);
-    testStack.push(testNode2);
-    
+    //test1.setScale(0.05, 0.05);
+    //test2.setScale(0.05, 0.05);
     
 //    while (window.isOpen()) {
 //
