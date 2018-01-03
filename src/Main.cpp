c49370347aef9719d15748799e100040bacf3ac7
Vince LaGrassa <vincent.lagrassa@gmail.com>
2018-01-03 01:45:55 -0500

    Move Stack and Queue tests to separate file

--- src/Main.cpp
+++ src/Main.cpp
@@ -23,69 +23,6 @@ int main() {
     //std::cout << test1 << "\n";
     //std::cout << test1.operator std::string() << "\n";
     
-    //Stack<MapPoint> testStack;
-    //testStack.push(test1);
-    //testStack.push(test2);
-    
-    //std::cout << "\nTop:\n" << *testStack.top << "\n";
-    //std::cout << "\nStack:\n" << testStack << "\n\n";
-    
-    //Node<MapPoint> topTemp = *testStack.top;
-    //std::cout << topTemp << "\n";
-    
-    Stack<int> intStack;
-    
-    intStack.push(*(new Node<int>(0)));
-    std::cout << intStack.pop() << "\n";
-    
-    intStack.push(*(new Node<int>(3)));
-    std::cout << intStack << "\n";
-    intStack.push(*(new Node<int>(2)));
-    std::cout << intStack << "\n";
-    intStack.push(*(new Node<int>(1)));
-    std::cout << intStack << "\n";
-    intStack.pop();
-    std::cout << intStack << "\n";
-    intStack.pop();
-    std::cout << intStack << "\n";
-    intStack.pop();
-    std::cout << intStack << "\n";
-    
-    intStack.push(3);
-    intStack.push(2);
-    intStack.push(1);
-    std::cout << intStack << "\n";
-    //std::cout << intStack.popData() << "\n";
-    //std::cout << intStack.popData() << "\n";
-    //std::cout << intStack.popData() << "\n";
-    
-    
-    
-    Queue<int> intQueue(intStack);
-    std::cout << intQueue << "\n";
-    
-    intQueue.enqueue(*(new Node<int>(4)));
-    std::cout << intQueue << "\n";
-    intQueue.enqueue(*(new Node<int>(5)));
-    std::cout << intQueue << "\n";
-    intQueue.enqueue(*(new Node<int>(6)));
-    std::cout << intQueue << "\n";
-    intQueue.dequeue();
-    std::cout << intQueue << "\n";
-    intQueue.dequeue();
-    std::cout << intQueue << "\n";
-    intQueue.dequeue();
-    std::cout << intQueue << "\n";
-    
-    intQueue.enqueue(4);
-    intQueue.enqueue(5);
-    intQueue.enqueue(6);
-    std::cout << intQueue.dequeue() << "\n";
-    std::cout << intQueue.dequeue() << "\n";
-    std::cout << intQueue.dequeue() << "\n";
-    
-    std::cout << intStack << "\n";
-    
     //sf::RenderWindow window(sf::VideoMode(1366, 768), "Latin Map Project");
     
     //sf::Texture defaultPoint;
