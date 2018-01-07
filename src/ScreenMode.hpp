a1ec78d76a91ea9296061ee69db34d76e4651058
Vince LaGrassa <vincent.lagrassa@gmail.com>
2018-01-07 03:27:39 -0500

    Create basic LinkedButton class

--- src/ScreenMode.hpp
+++ src/ScreenMode.hpp
@@ -30,4 +30,21 @@ private:
     
 };
 
+class LinkedButton : public sf::Sprite {
+public:
+    ScreenMode* link;
+    
+    LinkedButton() {};
+    LinkedButton(const LinkedButton& orig) : link(orig.link) {};
+    virtual ~LinkedButton() {};
+    
+    bool touchingMouse() {
+        return getGlobalBounds().contains(sf::Mouse().getPosition().x, sf::Mouse().getPosition().y);
+    }
+    
+    bool clicked() {
+        return touchingMouse() && sf::Mouse().isButtonPressed(sf::Mouse().Left);
+    }
+};
+
 #endif /* SCREENMODE_H */
