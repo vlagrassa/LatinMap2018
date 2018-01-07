c898bf735ba789d328ffbde5260d94fea10a6dda
Vince LaGrassa <vincent.lagrassa@gmail.com>
2018-01-07 15:00:05 -0500

    Clean up Main

--- src/Main.cpp
+++ src/Main.cpp
@@ -17,28 +17,13 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(1366, 768), "Latin Map Project");
     
     MapPoint test1(10, 10, "Test 1", "This is a test", 5, 5);
-    
-    sf::Vector2f testVector(27, 42);
-    MapPoint test2(testVector, "Test 2", "This is a test", 5, 5);
-    
+    MapPoint test2(27, 42, "Test 2", "This is a test", 5, 5);
     
     Stack<ScreenMode&> listOfScreens;
     ScreenMode testScreen(window);
     
     testScreen.createButton(new ScreenMode(window));
     listOfScreens.push(testScreen);
-    
-    std::cout << "Test Screen Address:  " << &testScreen << "\n";
-    std::cout << "Top of Stack Address: " << &(listOfScreens.top->data) << "\n";
-    
-    //std::cout << test1.getName() << ": " << test1.getDescription() << "; at (" << test1.getPosition().x << ", " << test1.getPosition().y << ")\n";
-    //std::cout << test2.getName() << ": " << test2.getDescription() << "; at (" << test2.getPosition().x << ", " << test2.getPosition().y << ")\n";
-    
-    //std::cout << test1 << "\n";
-    //std::cout << test1.operator std::string() << "\n";
-    
-    LinkedButton testButton(&testScreen, window);
-    
     
     //sf::Texture defaultPoint;
     //defaultPoint.loadFromFile("res/x.jpg"); //Image from https://www.freepik.com/free-icon/x-circle_692346.htm
@@ -66,14 +51,11 @@ int main() {
         }
         
         ScreenMode* nextScreen = listOfScreens.top->data.run();
-        //std::cout << "Next Screen: " << nextScreen << "\n";
         
         if (nextScreen == 0) {
             listOfScreens.pop();
         } else if (nextScreen != &listOfScreens.top->data) {
             listOfScreens.push(*nextScreen);
-        } else {
-            
         }
         
         window.clear(sf::Color::White);
@@ -84,11 +66,6 @@ int main() {
             window.draw(b);
         }
         
-        //std::cout << listOfScreens << "\n\n";
-        //std::cout << countdown << " runs left...\n";
-        
         window.display();
-        
-        countdown = 1;
     }
 }
