3cf14f8150496c5f5724707d5bbb5997276ec6e5
Vince LaGrassa <vincent.lagrassa@gmail.com>
2018-01-08 01:15:25 -0500

    Remove obsolete test MapPoints, fix initialization of listOfPoints

--- src/Main.cpp
+++ src/Main.cpp
@@ -31,9 +31,6 @@ int main() {
     
     std::cout << "Window is " << &window << "\n";
     
-    MapPoint test1(10, 10, "Test 1", "This is a test", 5, 5);
-    MapPoint test2(27, 42, "Test 2", "This is a test", 5, 5);
-    
     Stack<ScreenMode&> listOfScreens;
     
     ScreenMode testScreen1(window);
@@ -58,12 +55,6 @@ int main() {
     
     //sf::Texture defaultPoint;
     //defaultPoint.loadFromFile("res/x.jpg"); //Image from https://www.freepik.com/free-icon/x-circle_692346.htm
-    
-    //test1.setTexture(defaultPoint);
-    //test2.setTexture(defaultPoint);
-    
-    //test1.setScale(0.05, 0.05);
-    //test2.setScale(0.05, 0.05);
     
     while (window.isOpen()) {
 
@@ -114,9 +105,6 @@ int main() {
             }
         }
         
-        window.draw(test1);
-        window.draw(test2);
-        
         window.display();
     }
     
@@ -126,7 +114,7 @@ int main() {
 std::vector<MapPoint> buildMapPoints(std::string filename) {
     std::fstream                file(filename);
     std::string                 line;
-    std::vector<MapPoint>       listOfPoints();
+    std::vector<MapPoint>       listOfPoints;
     
     std::string                 tempName;
     std::vector<std::string>    tempAltNames;
