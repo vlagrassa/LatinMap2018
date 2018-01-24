195e21d52a0e63aa77a3f8838bab609e1f61b0e1
Vince LaGrassa <vincent.lagrassa@gmail.com>
2018-01-23 22:58:57 -0500

    Move MapPoint and PointScreen implementation to source

--- src/MapPoint.cpp
+++ src/MapPoint.cpp
@@ -1,5 +1,96 @@
 #include "MapPoint.hpp"
-#include <SFML/Graphics.hpp>
-#include <cstdlib>
-#include <iostream>
-#include <ctime>
+
+
+void PointScreen::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+    target.draw(popup);
+    ScreenMode::draw(target, states);
+};
+
+void PointScreen::initPopup() {
+    popup.setPosition(305, 205);
+    popup.setSize(sf::Vector2f(DEFAULT_WINDOW.getSize().x - 610, DEFAULT_WINDOW.getSize().y - 410));
+    popup.setOutlineColor(sf::Color::Black);
+    popup.setOutlineThickness(5);
+};
+
+
+void MapPoint::setText() {
+    if (names.alternate.size() > 0) link.addText(std::string("AKA ") + names.alternate, sf::Vector2f(315, 325));
+    link.addText(names.english, sf::Vector2f(315, 275));
+    link.addText(coords, sf::Vector2f(315, 415), 25);
+    //link.addText(description, sf::Vector2f(250, 265));
+}
+
+void MapPoint::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+    target.draw(outline);
+}
+
+void MapPoint::setAppearance() {
+    sf::CircleShape tempCircle(10);
+    tempCircle.setPosition(outline.getPosition());
+    switch (type) {
+        case (flumen):
+            tempCircle.setRotation(180);
+            tempCircle.move(20, 0);
+            tempCircle.setPointCount(3);
+            tempCircle.setOutlineThickness(3);
+            tempCircle.setOutlineColor(sf::Color(0, 0, 51));
+            tempCircle.setFillColor(sf::Color(51, 153, 255));
+            outline = tempCircle;
+            break;
+        case (insula):
+            tempCircle.setPointCount(4);
+            tempCircle.setRadius(5);
+            tempCircle.move(5, 5);
+            tempCircle.setOutlineThickness(2);
+            tempCircle.setOutlineColor(sf::Color(25, 51, 0));
+            tempCircle.setFillColor(sf::Color(0, 153, 76));
+            outline = tempCircle;
+            break;
+        case (mare):
+            tempCircle.setPointCount(20);
+            tempCircle.setOutlineThickness(2);
+            outline = tempCircle;
+            outline.setOutlineColor(sf::Color(0, 0, 51));
+            outline.setFillColor(sf::Color(51, 153, 255));
+            break;
+        case (mons):
+            tempCircle.setPointCount(3);
+            tempCircle.setOutlineThickness(3);
+            tempCircle.setOutlineColor(sf::Color(51, 25, 0));
+            tempCircle.setFillColor(sf::Color(153, 76, 0));
+            outline = tempCircle;
+            break;
+        case (provincia):
+            tempCircle.setPointCount(5);
+            tempCircle.setOutlineThickness(3);
+            tempCircle.setOutlineColor(sf::Color(0, 0, 51));
+            tempCircle.setFillColor(sf::Color(102, 0, 204));
+            outline = tempCircle;
+            break;
+        case (regio):
+            tempCircle.setPointCount(20);
+            tempCircle.setOutlineThickness(2);
+            tempCircle.setOutlineColor(sf::Color(25, 51, 0));
+            tempCircle.setFillColor(sf::Color(0, 153, 76));
+            outline = tempCircle;
+            break;
+        case (urbs):
+            tempCircle.setPointCount(4);
+             tempCircle.setRadius(5);
+            tempCircle.move(5, 5);
+            tempCircle.setOutlineThickness(2);
+            tempCircle.setOutlineColor(sf::Color(102, 0, 0));
+            tempCircle.setFillColor(sf::Color(204, 0, 102));
+            outline = tempCircle;
+            break;
+        case (via):
+            tempCircle.setRotation(180);
+            tempCircle.setPointCount(3);
+            tempCircle.setOutlineThickness(3);
+            tempCircle.setOutlineColor(sf::Color(102, 0, 0));
+            tempCircle.setFillColor(sf::Color(204, 0, 102));
+            outline = tempCircle;
+            break;
+    }
+}
--- src/MapPoint.hpp
+++ src/MapPoint.hpp
@@ -44,18 +44,10 @@ public:
     
     virtual ~PointScreen() {};
     
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
-        target.draw(popup);
-        ScreenMode::draw(target, states);
-    };
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
     
 private:
-    void initPopup() {
-        popup.setPosition(305, 205);
-        popup.setSize(sf::Vector2f(DEFAULT_WINDOW.getSize().x - 610, DEFAULT_WINDOW.getSize().y - 410));
-        popup.setOutlineColor(sf::Color::Black);
-        popup.setOutlineThickness(5);
-    }
+    void initPopup();
 
 };
 
@@ -86,12 +78,9 @@ public:
         return temp;
     };
     
-    void setText() {
-        if (names.alternate.size() > 0) link.addText(std::string("AKA ") + names.alternate, sf::Vector2f(315, 325));
-        link.addText(names.english, sf::Vector2f(315, 275));
-        link.addText(coords, sf::Vector2f(315, 415), 25);
-        //link.addText(description, sf::Vector2f(250, 265));
-    }
+    void setText();
+    
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
     
     /* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
      * Struct to hold names of the location.
@@ -122,84 +111,12 @@ public:
     
     const MapPointType type;
     
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
-        target.draw(outline);
-    }
-    
 private:
+    void setAppearance();
+    
     friend std::ostream& operator<<(std::ostream &strm, const MapPoint& p) {
         return strm << p.operator std::string();
     };
-    
-    void setAppearance() {
-        sf::CircleShape tempCircle(10);
-        tempCircle.setPosition(outline.getPosition());
-        switch (type) {
-            case (flumen):
-                tempCircle.setRotation(180);
-                tempCircle.move(20, 0);
-                tempCircle.setPointCount(3);
-                tempCircle.setOutlineThickness(3);
-                tempCircle.setOutlineColor(sf::Color(0, 0, 51));
-                tempCircle.setFillColor(sf::Color(51, 153, 255));
-                outline = tempCircle;
-                break;
-            case (insula):
-                tempCircle.setPointCount(4);
-                tempCircle.setRadius(5);
-                tempCircle.move(5, 5);
-                tempCircle.setOutlineThickness(2);
-                tempCircle.setOutlineColor(sf::Color(25, 51, 0));
-                tempCircle.setFillColor(sf::Color(0, 153, 76));
-                outline = tempCircle;
-                break;
-            case (mare):
-                tempCircle.setPointCount(20);
-                tempCircle.setOutlineThickness(2);
-                outline = tempCircle;
-                outline.setOutlineColor(sf::Color(0, 0, 51));
-                outline.setFillColor(sf::Color(51, 153, 255));
-                break;
-            case (mons):
-                tempCircle.setPointCount(3);
-                tempCircle.setOutlineThickness(3);
-                tempCircle.setOutlineColor(sf::Color(51, 25, 0));
-                tempCircle.setFillColor(sf::Color(153, 76, 0));
-                outline = tempCircle;
-                break;
-            case (provincia):
-                tempCircle.setPointCount(5);
-                tempCircle.setOutlineThickness(3);
-                tempCircle.setOutlineColor(sf::Color(0, 0, 51));
-                tempCircle.setFillColor(sf::Color(102, 0, 204));
-                outline = tempCircle;
-                break;
-            case (regio):
-                tempCircle.setPointCount(20);
-                tempCircle.setOutlineThickness(2);
-                tempCircle.setOutlineColor(sf::Color(25, 51, 0));
-                tempCircle.setFillColor(sf::Color(0, 153, 76));
-                outline = tempCircle;
-                break;
-            case (urbs):
-                tempCircle.setPointCount(4);
-                 tempCircle.setRadius(5);
-                tempCircle.move(5, 5);
-                tempCircle.setOutlineThickness(2);
-                tempCircle.setOutlineColor(sf::Color(102, 0, 0));
-                tempCircle.setFillColor(sf::Color(204, 0, 102));
-                outline = tempCircle;
-                break;
-            case (via):
-                tempCircle.setRotation(180);
-                tempCircle.setPointCount(3);
-                tempCircle.setOutlineThickness(3);
-                tempCircle.setOutlineColor(sf::Color(102, 0, 0));
-                tempCircle.setFillColor(sf::Color(204, 0, 102));
-                outline = tempCircle;
-                break;
-        }
-    }
 };
 
 #endif /* MAPPOINT_H */
